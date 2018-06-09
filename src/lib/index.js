4f022a2a4744fc135d9ba3057cb08b89019eb8ec
Enguerrand des Vaux <enguerrand@des-vaux.fr>
2018-06-09 14:13:43 +1000

    bye bye

--- src/lib/index.js
+++
@@ -1,6 +0,0 @@
-const EsriConfig = window.esriBundle.EsriConfig;
-const Map = window.esriBundle.Map;
-const MapView = window.esriBundle.MapView;
-const SceneView = window.esriBundle.SceneView;
-
-export { EsriConfig, Map, MapView, SceneView };
